7371244c00c94fdc552c5d146ab1a245b643427e
Eduard Carreras <ecarreras@gisce.net>
2017-11-21 12:45:54 +0100

    Implement reading socket with threading

--- reeprotocol/ip.py
+++ reeprotocol/ip.py
@@ -2,6 +2,8 @@
 """
 from __future__ import absolute_import
 import socket
+import queue
+import threading
 from .protocol import PhysicalLayer
 
 
@@ -14,24 +16,39 @@ class Ip(PhysicalLayer):
         """
         self.addr = addr
         self.connection = None
+        self.connected = False
+        self.queue = queue.Queue()
+        self.thread = threading.Thread(target=self.read_port)
 
     def connect(self):
         """Connect to `self.addr`
         """
         self.connection = socket.create_connection(self.addr)
-    
+        self.connected = True
+        self.thread.start()
+
     def disconnect(self):
         """Disconnects
         """
         if self.connection:
             self.connection.close()
-    
+        self.connected = False
+
+    def read_port(self):
+        """Read bytes from socket
+        """
+        while self.connected:
+            response = self.connection.recv(16)
+            if not response:
+                continue
+            for byte_resp in response:
+                self.queue.put(byte_resp)
+
     def send_byte(self, byte):
         """Send a byte"""
         assert isinstance(self.connection, socket.socket)
         self.connection.send(byte)
-    
-    def get_byte(self, timeout):
+
+    def get_byte(self, timeout=60):
         """Read a byte"""
-        assert isinstance(self.connection, socket.socket)
-        self.connection.recv(1)
+        return self.queue.get(True, timeout=timeout)
