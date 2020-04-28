b14a1f124812b185cc516943027082b59719ea04
Agustí Fita <afita@gisce.net>
2020-04-28 15:26:46 +0200

    Bump to v0.4.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('README.rst') as f:
 
 setup(
     name="iec870ree",
-    version="0.4.1",
+    version="0.4.2",
     author="GISCE-TI, S.L.",
     author_email="devel@gisce.net",
     description=("Library to connect and query information about electric"
